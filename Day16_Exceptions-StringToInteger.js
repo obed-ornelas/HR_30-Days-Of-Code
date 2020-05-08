b5f313e86cd52d725c32aeb7b37775da0f047eea
Obed <obed.j.ornelas@gmail.com>
2020-05-07 20:24:54 -0400

    Added comments and formatting

--- Day16_Exceptions-StringToInteger.js
+++ Day16_Exceptions-StringToInteger.js
@@ -1,21 +1,40 @@
+/*
+    OO 05-07-2020
+
+    Task
+    ----------------------------------------------------------------------------------------------------------
+    Read a string, 'S', and print its integer value; if 'S' cannot be converted to an integer,
+    print 'Bad String'
+
+    Sample Input
+    ----------------------------------------------------------------------------------------------------------
+    za
+
+    Sample Output
+    ----------------------------------------------------------------------------------------------------------
+    Bad String
+*/
+
+"use strict";
+
 process.stdin.resume();
-process.stdin.setEncoding('ascii');
+process.stdin.setEncoding("ascii");
 
 let input_stdin = "";
 let input_stdin_array = "";
 let input_currentline = 0;
 
-process.stdin.on('data', function (data) {
-    input_stdin += data;
+process.stdin.on("data", function (data) {
+  input_stdin += data;
 });
 
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();
+process.stdin.on("end", function () {
+  input_stdin_array = input_stdin.split("\n");
+  main();
 });
 
 function readLine() {
-    return input_stdin_array[input_currentline++];
+  return input_stdin_array[input_currentline++];
 }
 
 /////////////// ignore above this line ////////////////////
@@ -26,10 +45,10 @@ function readLine() {
  * `S` = read string from stdin.
  * try {
  *     `N` = numeric value of `S`.
- *     
+ *
  *     If `N` is `NaN` or `undefined`, then some exception must be thrown,
  *     you can write the following withtout using conditional statement:
- *     <condition to return true when `N` is `NaN` or `undefined`> && an_undefined_function_call()
+ *     <condition to return true when `N` is `NaN` or `undefined`> && an_undefined_function_call() // OO - Not a fan of this
  *
  *     print `N`
  * } catch (err) {
@@ -37,22 +56,22 @@ function readLine() {
  * }
  */
 
-function parseAndPrint(number){
-    try {
-        const parsed = parseInt(number, 10);
+function parseAndPrint(number) {
+  try {
+    const parsed = parseInt(number, 10);
 
-        if(isNaN(parsed)){
-            throw TypeError("not a number");
-        }
-
-        console.log(parsed);
-    } catch(e) {
-        console.error("Bad String");
+    if (isNaN(parsed)) {
+      throw TypeError("not a number");
     }
+
+    console.log(parsed);
+  } catch (e) {
+    console.error("Bad String");
+  }
 }
 
 function main() {
-    const s = readLine();
+  const s = readLine();
 
-    parseAndPrint(s);
+  parseAndPrint(s);
 }
