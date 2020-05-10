0438d032259ba46a8ba158bb36ebdd9199b4b1d1
Obed <obed.j.ornelas@gmail.com>
2020-05-09 23:56:33 -0400

    Day 18: Added logic and my personal changes

--- Day18_QueuesStacks.js
+++ Day18_QueuesStacks.js
@@ -1,9 +1,11 @@
+'use strict';
+
 process.stdin.resume();
 process.stdin.setEncoding('ascii');
 
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
+let input_stdin = "";
+let input_stdin_array = "";
+let input_currentline = 0;
 
 process.stdin.on('data', function (data) {
     input_stdin += data;
@@ -11,44 +13,62 @@ process.stdin.on('data', function (data) {
 
 process.stdin.on('end', function () {
     input_stdin_array = input_stdin.split("\n");
-    main();    
+    main();
 });
+
 function readLine() {
     return input_stdin_array[input_currentline++];
 }
 
-function Solution(){
-  //Write your code here
+function Solution() {
+  this.stack = [], this.queue = [];
+}
+
+Solution.prototype.pushCharacter = function (char) {
+    this.stack.push(char);
+}
+
+Solution.prototype.enqueueCharacter = function (char) {
+    this.queue.push(char);
+}
+
+Solution.prototype.popCharacter = function () {
+    return this.stack.pop();
+}
+
+Solution.prototype.dequeueCharacter = function () {
+    return this.queue.shift();
 }
 
 function main(){
     // read the string s
-    var s=readLine();
-    var len=s.length;
+    let s = readLine();
+    let len = s.length;
     // create the Solution class object p
-    var obj=new Solution();
+    const obj = new Solution();
     //push/enqueue all the characters of string s to stack
-    for(var i=0;i<len;i++){
+    for(let i = 0; i < len; i++) {
         obj.pushCharacter(s.charAt(i));
         obj.enqueueCharacter(s.charAt(i));
     }
   
-    var isPalindrome=true;
+    let isPalindrome = true;
     /*
     pop the top character from stack
     dequeue the first character from queue
     compare both the characters*/
 
-    for(var i=0;i<len/2;i++){
-        if(obj.popCharacter()!=obj.dequeueCharacter()){
-            isPalindrome=false;
-          	break;
+    for(let i=0; i < len/2; i++) {
+        if(obj.popCharacter() != obj.dequeueCharacter()){
+            isPalindrome = false;
+            break;
         }
     }
     //finally print whether string s is palindrome or not
 
-    if(isPalindrome)
-        console.log("The word, "+s+", is a palindrome.");    
-    else
-        console.log("The word, "+s+", is not a palindrome.");
+    if(isPalindrome) {
+        console.log("The word, " + s + ", is a palindrome.");
+    } else {
+        console.log("The word, " + s + ", is not a palindrome.");
+    }
 }
