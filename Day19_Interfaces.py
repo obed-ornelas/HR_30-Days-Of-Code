e84c0a7026331119b242293cac5a4bce8d4bd5fb
Obed <obed.j.ornelas@gmail.com>
2020-05-10 17:17:51 -0400

    Day 19: Finished Challenge (Interfaces)

--- Day19_Interfaces.py
+++ Day19_Interfaces.py
@@ -1,3 +1,26 @@
+"""
+  OO 05-10-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  The 'AdvancedArithmetic' interface and the method declaration for the 'abstract divisorSum(n)' method are
+  provided for you in the editor below.
+
+  Complete the implementation of 'Calculator' class, which implements the 'AdvancedArithmetic' interface.
+  The implementation for the divisorSum(n) method must return the sum of all divisors of 'n'
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  6
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  I implemented: AdvancedArithmetic
+  12
+
+"""
+
+
 class AdvancedArithmetic(object):
     def divisorSum(self, n):
         raise NotImplementedError
