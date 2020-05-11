7e2b656236a567e9de96dfbb83b59e554350c7d8
Obed <obed.j.ornelas@gmail.com>
2020-05-10 22:26:03 -0400

    Day 20: Finished Challenge (Sorting)

--- Day20_Sorting.js
+++ Day20_Sorting.js
@@ -1,3 +1,42 @@
+/*
+  OO 05-10-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  Given an array, 'a' , of size 'n' distinct elements, sort the array in ascending order using the Bubble Sort
+  algorithm above. Once sorted, print the following 3 lines:
+
+  1. Array is sorted in 'numSwaps' swaps.
+    where 'numSwaps' is the number of swaps that took place.
+  2. First Element: 'firstElement'
+    where 'firstElement' is the first element in the sorted array.
+  3. Last Element: 'lastElement'
+    where 'lastElement' is the last element in the sorted array.
+
+  Sample Input 0
+  ----------------------------------------------------------------------------------------------------------
+  3
+  1 2 3
+
+  Sample Output 0
+  ----------------------------------------------------------------------------------------------------------
+  Array is sorted in 0 swaps.
+  First Element: 1
+  Last Element: 3
+
+  Sample Input 1
+  ----------------------------------------------------------------------------------------------------------
+  3
+  3 2 1
+
+  Sample Output 1
+  ----------------------------------------------------------------------------------------------------------
+  Array is sorted in 3 swaps.
+  First Element: 1
+  Last Element: 3
+
+*/
+
 "use strict";
 
 process.stdin.resume();
