b485ea6a72e5c3af9e0e9ac3625cb9c87ca1aabe
Obed <obed.j.ornelas@gmail.com>
2020-05-12 16:37:46 -0400

    Day 24: Finished Challenge (More Linked Lists)

--- Day24_MoreLinkedLists.js
+++ Day24_MoreLinkedLists.js
@@ -1,3 +1,43 @@
+/*
+  OO 05-12-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  A Node class is provided for you in the editor. A Node object has an integer data field, 'data' , and a Node
+  instance pointer, 'next', pointing to another node (i.e.: the next node in a list).
+
+  A 'removeDuplicates' function is declared in your editor, which takes a pointer to the 'head' node of a
+  linked list as a parameter. Complete 'removeDuplicates' so that it deletes any duplicate nodes from the
+  list and returns the head of the updated list.
+
+  Note: The 'head' pointer may be null, indicating that the list is empty. Be sure to reset your 'next' pointer
+  when performing deletions to avoid breaking the list.
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  You do not need to read any input from stdin. The following input is handled by the locked stub code and
+  passed to the 'removeDuplicates' function:
+
+  The first line contains an integer, 'N', the number of nodes to be inserted.
+  The 'N' subsequent lines each contain an integer describing the 'data' value of a node being inserted at
+  the list's tail.
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  6
+  1
+  2
+  2
+  3
+  3
+  4
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  1 2 3 4
+
+*/
+
 "use strict";
 
 process.stdin.resume();
