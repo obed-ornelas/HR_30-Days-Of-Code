423ccfc042f3c25d739f5d345d292ef9153642a7
Obed <obed.j.ornelas@gmail.com>
2020-05-11 22:19:15 -0400

    Day 22: Hit file with formatter

--- Day22_BinarySearchTrees.js
+++ Day22_BinarySearchTrees.js
@@ -1,70 +1,72 @@
 "use strict";
 
-// Start of function Node
 function Node(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-}; // End of function Node
+  this.data = data;
+  this.left = null;
+  this.right = null;
+}
 
-// Start of function BinarySearchTree
 function BinarySearchTree() {
-    this.insert = function(root, data) {
-        if (root === null) {
-            this.root = new Node(data);
-            
-            return this.root;
-        }
-        
-        if (data <= root.data) {
-            if (root.left) {
-                this.insert(root.left, data);
-            } else {
-                root.left = new Node(data);
-            }
-        } else {
-            if (root.right) {
-                this.insert(root.right, data);
-            } else {
-                root.right = new Node(data);
-            }
-        }
-        
-        return this.root;
-    };
-    
-    this.getHeight = function(root) {
-        if (!root) {
-            return -1;
-        }
+  this.insert = function (root, data) {
+    if (root === null) {
+      this.root = new Node(data);
 
-        const height = 1 + Math.max(this.getHeight(root.left), this.getHeight(root.right));
-        return height;
-    };
-};
+      return this.root;
+    }
+
+    if (data <= root.data) {
+      if (root.left) {
+        this.insert(root.left, data);
+      } else {
+        root.left = new Node(data);
+      }
+    } else {
+      if (root.right) {
+        this.insert(root.right, data);
+      } else {
+        root.right = new Node(data);
+      }
+    }
+
+    return this.root;
+  };
+
+  this.getHeight = function (root) {
+    if (!root) {
+      return -1;
+    }
+
+    const height =
+      1 + Math.max(this.getHeight(root.left), this.getHeight(root.right));
+    return height;
+  };
+}
 
 process.stdin.resume();
-process.stdin.setEncoding('ascii');
+process.stdin.setEncoding("ascii");
 
 let _input = "";
 
-process.stdin.on('data', function (data) {
-    _input += data;
+process.stdin.on("data", function (data) {
+  _input += data;
 });
 
-process.stdin.on('end', function () {
-    main();
+process.stdin.on("end", function () {
+  main();
 });
 
 function main() {
-    const tree = new BinarySearchTree();
-    let root = null;
+  const tree = new BinarySearchTree();
+  let root = null;
 
-    const values = _input.split('\n').filter(input => input).map(Number);
+  const values = _input
+    .split("\n")
+    .filter((input) => input)
+    .map(Number);
 
-    for (let i = 1; i < values.length; i++) {
-        root = tree.insert(root, values[i]);
-    }
-    
-    console.log(tree.getHeight(root));
+  for (let i = 1; i < values.length; i++) {
+    root = tree.insert(root, values[i]);
+  }
+
+  console.log(tree.getHeight(root));
 }
