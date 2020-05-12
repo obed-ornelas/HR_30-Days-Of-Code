66b6e3ca3cf04b5473bc3e3a04cff33b3fb64fe0
Obed <obed.j.ornelas@gmail.com>
2020-05-11 22:25:13 -0400

    Day 22: Finished Challenge (Binary Search Trees)

--- Day22_BinarySearchTrees.js
+++ Day22_BinarySearchTrees.js
@@ -1,3 +1,36 @@
+/*
+  OO 05-11-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  The height of a binary search tree is the number of edges between the tree's root and its furthest leaf.
+  You are given a pointer, 'root', pointing to the root of a binary search tree. Complete the 'getHeight'
+  function provided in your editor so that it returns the height of the binary search tree.
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  The locked stub code in your editor reads the following inputs and assembles them into a binary search tree:
+  The first line contains an integer, 'n', denoting the number of nodes in the tree.
+  Each of the 'n' subsequent lines contains an integer, 'data', denoting the value of an element that must
+  be added to the BST.
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  7
+  3
+  5
+  2
+  1
+  4
+  6
+  7
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  3
+
+*/
+
 "use strict";
 
 function Node(data) {
