de562d7688f8a33a65392dd2ae2532f1675db4f1
Obed <obed.j.ornelas@gmail.com>
2020-05-12 16:31:24 -0400

    Day 24: Hit file with formatter

--- Day24_MoreLinkedLists.js
+++ Day24_MoreLinkedLists.js
@@ -8,89 +8,88 @@ let input_stdin_array = "";
 let input_currentline = 0;
 
 process.stdin.on("data", function (data) {
-    input_stdin += data;
+  input_stdin += data;
 });
 
 process.stdin.on("end", function () {
-    input_stdin_array = input_stdin.split("\n").filter(input => input);
-    main();
+  input_stdin_array = input_stdin.split("\n").filter((input) => input);
+  main();
 });
 
 function readLine() {
-    return input_stdin_array[input_currentline++];
+  return input_stdin_array[input_currentline++];
 }
 
-function Node(data){
-    this.data = data;
-    this.next = null;
+function Node(data) {
+  this.data = data;
+  this.next = null;
 }
 
-function Solution(){
+function Solution() {
+  this.removeDuplicates = function (head) {
+    const distinct = [];
 
-    this.removeDuplicates = function(head) {
-        const distinct = [];
+    let previousNode = null;
+    let currentNode = head;
 
-        let previousNode = null;
-        let currentNode = head;
+    while (currentNode) {
+      if (distinct.indexOf(currentNode.data) === -1) {
+        distinct.push(currentNode.data);
 
-        while(currentNode){
-            if(distinct.indexOf(currentNode.data) === -1){
-                distinct.push(currentNode.data);
+        previousNode = currentNode;
+      } else {
+        previousNode.next = currentNode.next;
+      }
 
-                previousNode = currentNode;
-            } else {
-                previousNode.next = currentNode.next;
-            }
-
-            currentNode = currentNode.next;
-        }
-
-        return head;
+      currentNode = currentNode.next;
     }
 
-	this.insert = function(head, data) {
-        const node = new Node(data);
+    return head;
+  };
 
-        if(head == null) {
-            head = node;
-        } else if(head.next == null) {
-            head.next = node;
-        } else {
-            let start = head;
+  this.insert = function (head, data) {
+    const node = new Node(data);
 
-            while(start.next != null) {
-                start = start.next;
-            }
+    if (head == null) {
+      head = node;
+    } else if (head.next == null) {
+      head.next = node;
+    } else {
+      let start = head;
 
-            start.next = node;
-        }
+      while (start.next != null) {
+        start = start.next;
+      }
 
-        return head;
-    };
-
-	this.display = function(head){
-        let start = head;
-        const toDisplay = [];
-
-        while(start){
-            toDisplay.push(start.data);
-            start = start.next;
-        }
-
-        console.log(toDisplay.map(int => int.toString()).join(" "));
-    };
-}
-
-function main(){
-    const T = parseInt(readLine());
-    let head = null;
-    const mylist = new Solution();
-
-    for(let i = 0; i < T; i++){
-        let data = parseInt(readLine());
-        head = mylist.insert(head,data);
+      start.next = node;
     }
 
-    head = mylist.removeDuplicates(head);
-    mylist.display(head);
+    return head;
+  };
+
+  this.display = function (head) {
+    let start = head;
+    const toDisplay = [];
+
+    while (start) {
+      toDisplay.push(start.data);
+      start = start.next;
+    }
+
+    console.log(toDisplay.map((int) => int.toString()).join(" "));
+  };
+}
+
+function main() {
+  const T = parseInt(readLine());
+  let head = null;
+  const mylist = new Solution();
+
+  for (let i = 0; i < T; i++) {
+    let data = parseInt(readLine());
+    head = mylist.insert(head, data);
+  }
+
+  head = mylist.removeDuplicates(head);
+  mylist.display(head);
 }
