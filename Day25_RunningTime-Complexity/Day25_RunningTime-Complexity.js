d63568c1da5438e0b05883227fb5ab769400e529
Obed <obed.j.ornelas@gmail.com>
2020-05-13 17:39:39 -0400

    Day 25: Hit file with formatter

--- Day25_RunningTime-Complexity/Day25_RunningTime-Complexity.js
+++ Day25_RunningTime-Complexity/Day25_RunningTime-Complexity.js
@@ -1,19 +1,19 @@
 "use strict";
 
 function isPrime(number) {
-    if (number === 1) {
-        console.log("Not prime");
-        return
-    }
+  if (number === 1) {
+    console.log("Not prime");
+    return;
+  }
 
-    for (let i = 2; i <= Math.sqrt(number); i++){
-        if(number % i === 0){
-            console.log("Not prime");
-            return;
-        }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      console.log("Not prime");
+      return;
     }
+  }
 
-    console.log("Prime");
+  console.log("Prime");
 }
 
 process.stdin.resume();
@@ -24,23 +24,23 @@ let inputSplit;
 let lineCounter = 0;
 
 process.stdin.on("data", function (stdin) {
-    input += stdin;
+  input += stdin;
 });
 
 process.stdin.on("end", function () {
-    inputSplit = input.split("\n").filter(string => string);
-    main();
+  inputSplit = input.split("\n").filter(string => string);
+  main();
 });
 
 function readLine() {
-    return inputSplit[lineCounter++];
+  return inputSplit[lineCounter++];
 }
 
-function main(){
-    const testCases = parseInt(readLine(), 10);
-    
-    for (let i = 0; i < testCases; i++){
-        const number = parseInt(readLine(), 10);
-        isPrime(number);
-    }
+function main() {
+  const testCases = parseInt(readLine(), 10);
+
+  for (let i = 0; i < testCases; i++) {
+    const number = parseInt(readLine(), 10);
+    isPrime(number);
+  }
 }
