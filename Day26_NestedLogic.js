6743327333aa8f29bf0c959bb0f852a06a10c4c2
Obed <obed.j.ornelas@gmail.com>
2020-05-14 16:50:44 -0400

    Day 26: Added logic and personal changes

--- Day26_NestedLogic.js
+++ Day26_NestedLogic.js
@@ -1,14 +1,53 @@
-function processData(input) {
-    //Enter your code here
-} 
+"use strict";
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
+
+let input = "";
+let inputSplit = [];
+let lineCounter = 0;
+
+process.stdin.on("data", function (stdin) {
+    input += stdin;
 });
 
 process.stdin.on("end", function () {
-   processData(_input);
+    inputSplit = input.split("\n").filter(string => string);
+   main();
 });
+
+function readLine() {
+    return inputSplit[lineCounter++];
+}
+
+function parseDate(date){
+    const [day, month, year] = date.split(" ").map(string => parseInt(string, 10));
+    return { day, month, year };
+}
+
+function calculateFine(returnDate, dueDate) {
+    if (returnDate.year > dueDate.year) {
+        return 10000;
+    }
+
+    let fine = 0;
+
+    if (returnDate.year === dueDate.year) {
+        if(returnDate.month > dueDate.month) {
+            fine = 500 * (returnDate.month - dueDate.month);
+        } else if (returnDate.month === dueDate.month) {
+            if (returnDate.day > dueDate.day) {
+                fine = 15 * (returnDate.day - dueDate.day);
+            }
+        }
+    }
+
+    return fine;
+}
+
+function main() {
+    const returnDate = parseDate(readLine());
+    const dueDate = parseDate(readLine());
+
+    console.log(calculateFine(returnDate, dueDate));
+}
