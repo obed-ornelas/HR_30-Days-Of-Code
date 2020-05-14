dabb4427f3977311430f3c3849418cc70c322d20
Obed <obed.j.ornelas@gmail.com>
2020-05-14 16:53:50 -0400

    Day 26: Hit file with formatter

--- Day26_NestedLogic.js
+++ Day26_NestedLogic.js
@@ -8,46 +8,49 @@ let inputSplit = [];
 let lineCounter = 0;
 
 process.stdin.on("data", function (stdin) {
-    input += stdin;
+  input += stdin;
 });
 
 process.stdin.on("end", function () {
-    inputSplit = input.split("\n").filter(string => string);
-   main();
+  inputSplit = input.split("\n").filter(string => string);
+
+  main();
 });
 
 function readLine() {
-    return inputSplit[lineCounter++];
+  return inputSplit[lineCounter++];
 }
 
-function parseDate(date){
-    const [day, month, year] = date.split(" ").map(string => parseInt(string, 10));
-    return { day, month, year };
+function parseDate(date) {
+  const [day, month, year] = date
+    .split(" ")
+    .map(string => parseInt(string, 10));
+  return { day, month, year };
 }
 
 function calculateFine(returnDate, dueDate) {
-    if (returnDate.year > dueDate.year) {
-        return 10000;
+  if (returnDate.year > dueDate.year) {
+    return 10000;
+  }
+
+  let fine = 0;
+
+  if (returnDate.year === dueDate.year) {
+    if (returnDate.month > dueDate.month) {
+      fine = 500 * (returnDate.month - dueDate.month);
+    } else if (returnDate.month === dueDate.month) {
+      if (returnDate.day > dueDate.day) {
+        fine = 15 * (returnDate.day - dueDate.day);
+      }
     }
+  }
 
-    let fine = 0;
-
-    if (returnDate.year === dueDate.year) {
-        if(returnDate.month > dueDate.month) {
-            fine = 500 * (returnDate.month - dueDate.month);
-        } else if (returnDate.month === dueDate.month) {
-            if (returnDate.day > dueDate.day) {
-                fine = 15 * (returnDate.day - dueDate.day);
-            }
-        }
-    }
-
-    return fine;
+  return fine;
 }
 
 function main() {
-    const returnDate = parseDate(readLine());
-    const dueDate = parseDate(readLine());
+  const returnDate = parseDate(readLine());
+  const dueDate = parseDate(readLine());
 
-    console.log(calculateFine(returnDate, dueDate));
+  console.log(calculateFine(returnDate, dueDate));
 }
