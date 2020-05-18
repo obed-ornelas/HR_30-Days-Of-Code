c46e9456b486ade2591c8ce9195eacc709be904b
Obed <obed.j.ornelas@gmail.com>
2020-05-18 13:46:16 -0400

    Day 28: Finished Challenge (RegEx, Patterns, and Intro to Databases)

--- Day28_Regex-Patterns-Databases.js
+++ Day28_Regex-Patterns-Databases.js
@@ -1,3 +1,38 @@
+/*
+  OO 05-18-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  Consider a database table, Emails, which has the attributes First Name and Email ID. Given 'N' rows of
+  data simulating the Emails table, print an alphabetically-ordered list of people whose email address ends
+  in '@gmail.com'
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  The first line contains an integer, 'N', total number of rows in the table.
+  Each of the 'N' subsequent lines contains 2 space-separated strings denoting a person's first name and
+  email ID, respectively.
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  6
+  riya riya@gmail.com
+  julia julia@julia.me
+  julia sjulia@gmail.com
+  julia julia@gmail.com
+  samantha samantha@gmail.com
+  tanya tanya@gmail.com
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  julia
+  julia
+  riya
+  samantha
+  tanya
+
+*/
+
 "use strict";
 
 process.stdin.resume();
@@ -6,15 +41,15 @@ process.stdin.setEncoding("utf-8");
 let inputString = "";
 let currentLine = 0;
 
-process.stdin.on("data", (stdin) => {
+process.stdin.on("data", stdin => {
   inputString += stdin;
 });
 
-process.stdin.on("end", (_) => {
+process.stdin.on("end", () => {
   inputString = inputString
     .replace(/\s*$/, "")
     .split("\n")
-    .map((str) => str.replace(/\s*$/, ""));
+    .map(str => str.replace(/\s*$/, ""));
 
   main();
 });
