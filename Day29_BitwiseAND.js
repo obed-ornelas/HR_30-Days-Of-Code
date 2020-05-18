06a47456112ae66b2386aec252a4312c057f56b8
Obed <obed.j.ornelas@gmail.com>
2020-05-18 16:35:28 -0400

    Day 29: Added logic and personal changes

--- Day29_BitwiseAND.js
+++ Day29_BitwiseAND.js
@@ -6,11 +6,11 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on('data', stdin => {
+    inputString += stdin;
 });
 
-process.stdin.on('end', _ => {
+process.stdin.on('end', () => {
     inputString = inputString.replace(/\s*$/, '')
         .split('\n')
         .map(str => str.replace(/\s*$/, ''));
@@ -22,16 +22,39 @@ function readLine() {
     return inputString[currentLine++];
 }
 
+function makeSet(max){
+    const set = [];
 
+    for (let i = 1; i <= max; i++ ){
+        set.push(i);
+    }
+
+    return set;
+}
+
+function findLowestBitwiseAnd(setMax, limit) {
+    const set = makeSet(setMax);
+    let lowestAnd = 0;
+
+    for(let i = 0; i < set.length - 1; i++){
+        for(let j = i + 1; j < set.length; j++) {
+            const bitwiseAnd = set[i] & set[j];
+
+            if(bitwiseAnd < limit && bitwiseAnd > lowestAnd) {
+                lowestAnd = bitwiseAnd;
+            }
+        }
+    }
+
+    return lowestAnd;
+}
 
 function main() {
-    const t = parseInt(readLine(), 10);
+    const tests = parseInt(readLine(), 10);
 
-    for (let tItr = 0; tItr < t; tItr++) {
-        const nk = readLine().split(' ');
+    for (let i = 0; i < tests; i++) {
+        const [n, k] = readLine().split(' ');
 
-        const n = parseInt(nk[0], 10);
-
-        const k = parseInt(nk[1], 10);
+        console.log(findLowestBitwiseAnd(n, k));
     }
 }
