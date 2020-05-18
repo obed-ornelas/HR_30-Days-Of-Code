5817c1c6624afcc55085568b8f38a41c2968843a
Obed <obed.j.ornelas@gmail.com>
2020-05-18 13:40:23 -0400

    Day 28: Hit file with formatter

--- Day28_Regex-Patterns-Databases.js
+++ Day28_Regex-Patterns-Databases.js
@@ -1,54 +1,55 @@
-'use strict';
+"use strict";
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', stdin => {
-    inputString += stdin;
+process.stdin.on("data", (stdin) => {
+  inputString += stdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 function isGmailAccount(email) {
-    const [_, domain] = email.split('@');
+  const [_, domain] = email.split("@");
 
-    if (domain === 'gmail.com'){
-        return true;
-    }
+  if (domain === "gmail.com") {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 function printUsers(users) {
-    for (let i = 0; i < users.length; i++) {
-        console.log(users[i]);
-    }
+  for (let i = 0; i < users.length; i++) {
+    console.log(users[i]);
+  }
 }
 
 function main() {
-    const rows = parseInt(readLine(), 10);
-    const users = [];
+  const rows = parseInt(readLine(), 10);
+  const users = [];
 
-    for (let i = 0; i < rows; i++) {
-        const [firstName, emaiID] = readLine().split(' ');
+  for (let i = 0; i < rows; i++) {
+    const [firstName, emaiID] = readLine().split(" ");
 
-        if (isGmailAccount(emaiID)){
-            users.push(firstName);
-        }
+    if (isGmailAccount(emaiID)) {
+      users.push(firstName);
     }
+  }
 
-    printUsers(users.sort());
+  printUsers(users.sort());
 }
