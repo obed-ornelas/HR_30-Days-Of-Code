2666c5ef08ae3bd0c197288a1423ff07a07ad073
Obed <obed.j.ornelas@gmail.com>
2020-05-18 20:21:28 -0400

    Day 0: Adding Finished Challenge (Hello, World)

---
+++ Day0_HelloWorld.js
@@ -0,0 +1,28 @@
+/*
+  OO 05-18-2020
+
+  Task
+  ----------------------------------------------------------------------------------------------------------
+  To complete this challenge, you must save a line of input from stdin to a variable, print 'Hello, World.'
+  on a single line, and finally print the value of your variable on a second line.
+
+  Input Format
+  ----------------------------------------------------------------------------------------------------------
+  A single line of text denoting 'inputString' (the variable whose contents must be printed).
+
+  Sample Input
+  ----------------------------------------------------------------------------------------------------------
+  Welcome to 30 Days of Code!
+
+  Sample Output
+  ----------------------------------------------------------------------------------------------------------
+  Hello, World. 
+  Welcome to 30 Days of Code!
+
+*/
+
+function processData(inputString) {
+  console.log("Hello, World.");
+
+  console.log(inputString);
+}
